fcb4d77a09aed72907aefb2c7475053bc68dcc89
Andrei Cramariuc <andrei.cramariuc@gmail.com>
2024-08-15 17:39:42 +0200

    Switching to using compslam

--- test/translator_test.py
+++ test/translator_test.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
 import rospy
 import numpy as np
 from nav_msgs.msg import Odometry
-from alma_msgs.msg import AlmaState
 
 class Translator:
     def __init__(self):
@@ -14,18 +12,19 @@ class Translator:
         self.pub = rospy.Publisher('/racetrack/odom_passthrough', Odometry, queue_size=20)
         
         # Subscriber
-        self.sub = rospy.Subscriber('/state_estimator/alma_state', AlmaState, self.alma_state_callback)
+        self.sub = rospy.Subscriber('/compslam_lio/odometry', Odometry, self.compslam_callback)
 
         self.covariance = np.eye(6).flatten() * 0.01
 
-    def alma_state_callback(self, alma_msg):
+    def compslam_callback(self, alma_msg):
         odom_msg = Odometry()
-        odom_msg.header = alma_msg.header
+        odom_msg.header.stamp = alma_msg.header.stamp
+        odom_msg.header.frame_id = "map"
+        odom_msg.child_frame_id = "base"
         odom_msg.pose.pose = alma_msg.pose.pose
         odom_msg.pose.covariance = self.covariance
-        odom_msg.twist.twist = alma_msg.twist.twist
-        odom_msg.twist.covariance = self.covariance
-        odom_msg.child_frame_id = "base"
+        # odom_msg.twist.twist = alma_msg.twist.twist
+        # odom_msg.twist.covariance = self.covariance
 
         self.pub.publish(odom_msg)
 
